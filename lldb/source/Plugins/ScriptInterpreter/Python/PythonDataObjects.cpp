ae20ead837f82ff26687f94bac17ee7fd872ca5f
Martin Storsjö <martin@martin.st>
2020-02-05 22:29:59 +0200

    Keep it compatible with python 3.5.

--- lldb/source/Plugins/ScriptInterpreter/Python/PythonDataObjects.cpp
+++ lldb/source/Plugins/ScriptInterpreter/Python/PythonDataObjects.cpp
@@ -834,7 +834,7 @@ def main(f):
                       Parameter.VAR_KEYWORD):
             pass
         else:
-            raise Exception(f'unknown parameter kind: {kind}')
+            raise Exception('unknown parameter kind: ' + kind)
     return ArgInfo(count, varargs)
 )";
 #endif
