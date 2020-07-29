tromey/lldb

Add a missing TypeAndOrName constructor
Add a TypeAndOrName constructor that was declared but not defined.
This is used in the Rust plugin.  See https://reviews.llvm.org/D44752

source/Symbol/Type.cpp
@@ -699,6 +699,13 @@ TypeAndOrName::TypeAndOrName(const TypeAndOrName &rhs)
 TypeAndOrName::TypeAndOrName(ConstString &in_type_const_string)
     : m_type_name(in_type_const_string) {}
 
+TypeAndOrName::TypeAndOrName(const CompilerType &type)
+    : m_type_pair(type)
+{
+  if (m_type_pair)
+    m_type_name = m_type_pair.GetName();
+}
+
 TypeAndOrName &TypeAndOrName::operator=(const TypeAndOrName &rhs) {
   if (this != &rhs) {
     m_type_name = rhs.m_type_name;

vadimcn/llvm-project

lldb/source/Symbol/Type.cpp
@@ -735,6 +735,13 @@ TypeAndOrName::TypeAndOrName(const char *in_type_str)
 TypeAndOrName::TypeAndOrName(ConstString &in_type_const_string)
     : m_type_name(in_type_const_string) {}
 
+TypeAndOrName::TypeAndOrName(const CompilerType &type)
+    : m_compiler_type(type)
+{
+  if (m_compiler_type)
+    m_type_name = m_compiler_type.GetTypeName();
+}
+
 bool TypeAndOrName::operator==(const TypeAndOrName &other) const {
   if (m_compiler_type != other.m_compiler_type)
     return false;