92466d28789bcd409acf4874946755b204da9ed7
Vadim Chugunov <vadimcn@gmail.com>
2021-01-31 15:20:20 -0800

    Python build.

--- toolchain-arm-linux-gnueabihf.cmake
+++
@@ -1,6 +0,0 @@
-set(TARGET_TRIPLE arm-linux-gnueabihf)
-set(CMAKE_SYSTEM_NAME Linux)
-set(CMAKE_SYSTEM_PROCESSOR arm)
-set(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc)
-set(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++)
-set(CMAKE_STRIP arm-linux-gnueabihf-strip)
